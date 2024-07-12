0feedcde099c27013cad9d4e7737aeaa441038c1
Jatin Kumar <Jatin.Kumar@asl.mum.sst>
2024-07-12 18:56:35 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pie = 3.14;
+            int radius;
+            Console.WriteLine("Enter the value of radius");
+            radius=Convert.ToInt32(Console.ReadLine());
+            double result = 2 * pie * radius;
+            Console.WriteLine("The circumference of given radius of a circle is "+result);
+
+        }
+    }
+}
